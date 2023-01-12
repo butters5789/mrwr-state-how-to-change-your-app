8187fb775446685621b15c9ea2fe0b3ec39ba195
butters5789 <butters5789@gmail.com>
2023-01-12 13:07:30 -0700

    event and state example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import AnimalShow from "./AnimalShow";
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Number of animals: {count}</div>
 
       <AnimalShow />
     </div>
